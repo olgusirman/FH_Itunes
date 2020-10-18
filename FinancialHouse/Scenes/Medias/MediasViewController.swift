6a26603e9f18d79bc40519b96dcd04e20883afef
Olgu SIRMAN <o.sirman@emakina.com.tr>
2020-10-18 22:16:15 +0100

    MediasController setup added

--- FinancialHouse/Scenes/Medias/MediasViewController.swift
+++ FinancialHouse/Scenes/Medias/MediasViewController.swift
@@ -28,6 +28,18 @@ final class MediasViewController: BaseViewController, MediasDisplayLogic {
     
     // MARK: Setup
     
+    // MARK: Object lifecycle
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
     private func setup() {
         let viewController = self
         let interactor = MediasInteractor()
